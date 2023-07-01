67ab4e7b3787c88826643d535bda45f0cb3a34f1
Paul <paul.ivanov1990@gmail.com>
2023-07-01 23:45:14 +0300

    delete api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { useEffect } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
-import * as api from './utils/Api';
 
 import Header from './components/Header.jsx';
 import Search from './components/Search.jsx';
 import Trends from './components/Trends.jsx';
 
 function App() {
-  // useEffect(() => {
-  //   api.random().then((response) => {
-  //     console.log(response);
-  //   });
-  // }, []);
   return (
     <div className='page'>
       <Header />
