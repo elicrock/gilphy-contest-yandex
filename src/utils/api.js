0243af28072b24be45a3f8394ee6ef4f69e094a5
Viktor Filippov <mozgfoxtrot@gmail.com>
2023-07-02 00:28:43 +0400

    create api

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ function resStatus(res) {
 }
 
 function request(url, options) {
-  return fetch(url, options).then(resStatus)
+  return fetch(url, options).then(resStatus);
 }
 
 export const BASE_URL = 'http://api.giphy.com/v1/gifs';
@@ -14,33 +14,14 @@ export const API_KEY = 'SQHURABMQ01GCNGOXpzyEAxQu8ox3Rgw';
 
 export const search = (query) => {
   return request(`${BASE_URL}/search?q=${query}&api_key=${API_KEY}&limit=9`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({query}),
-  })
-}
+    body: JSON.stringify({ query }),
+  });
+};
 
 export const trending = () => {
-  return request(`${BASE_URL}/trending&api_key=${API_KEY}&limit=9`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({}),
-  })
-}
+  return request(`${BASE_URL}/trending?&api_key=${API_KEY}&limit=9`);
+};
 
 export const random = () => {
-  return request(`${BASE_URL}/trending&api_key=${API_KEY}`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({}),
-  })
-}
+  return request(`${BASE_URL}/random?&api_key=${API_KEY}`);
+};
